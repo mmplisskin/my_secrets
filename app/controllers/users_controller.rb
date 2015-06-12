e9e433e65655dbf96a6d8a60df7a44f500eb5f90
Busyra Anwar <busyra@me.com>
2015-06-12 14:39:15 -0700

    adds new and create function in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,16 @@ class UsersController < ApplicationController
 	end
 
 	def new
+		@user = User.ner
 	end
 
 	def create
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to root_path, notice: "Successfully signed up!"
+		else
+			render :new
+		end
 	end
 
 end
