847c9d9a3adcf3e289cacfd872b98da535d345a2
mmplisskin <mmplisskin@gmail.com>
2015-06-13 13:44:03 -0700

    adds model relationships

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+include SessionsHelper
+
+  # def authorized?
+  #     redirect_to root_path unless current_user
+  #
+  # end
   protect_from_forgery with: :exception
 end
