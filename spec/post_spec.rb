f1da8cbd8171d6aef95c27a925d51e8479012d6a
Busyra Anwar <busyra@me.com>
2015-06-18 10:27:37 -0700

    adds test for description, validation, and format .rspec

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -1,9 +1,15 @@
 require "rails_helper"
 
 RSpec.describe Post, type: :model do
-	it "is invalid without a title" do
+	it "is an invalid post without a title" do
 		post = Post.new(title: nil)
     	expect(post).to be_invalid
   	end 
 
+  	it "is an invalid post without a description" do
+  		post= Post.new(description: nil )
+  		expect(post).to be_invalid
+  	end
+
 end
+
