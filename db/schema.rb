f1b40d13e212fd8a0af1bd9a88becc95fd04c7f5
Busyra Anwar <busyra@me.com>
2015-06-19 16:19:28 -0700

    fixed migration issues

--- db/schema.rb
+++ db/schema.rb
@@ -24,8 +24,11 @@ ActiveRecord::Schema.define(version: 20150619182935) do
     t.string   "location"
     t.string   "image_url"
     t.string   "url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "last_update"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "post_recipients", force: :cascade do |t|
@@ -41,8 +44,7 @@ ActiveRecord::Schema.define(version: 20150619182935) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.string   "description"
-    t.integer  "user_id"
-    t.datetime "last_update"
+    t.integer  "ouser_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
@@ -50,18 +52,14 @@ ActiveRecord::Schema.define(version: 20150619182935) do
   create_table "recipients", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
+    t.integer  "ouser_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
+  add_index "recipients", ["ouser_id"], name: "index_recipients_on_ouser_id", using: :btree
 
   add_foreign_key "post_recipients", "posts"
   add_foreign_key "post_recipients", "recipients"
+  add_foreign_key "recipients", "ousers"
 end
