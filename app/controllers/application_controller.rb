edcbed4f55934781814245b9b53d6a8ee5c22fcc
Busyra Anwar <busyra@me.com>
2015-06-19 11:47:16 -0700

    updates session controller and added ouser model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-include SessionsHelper
+  	protect_from_forgery with: :exception
 
-  # def authorized?
-  #     redirect_to root_path unless current_user
-  #
-  # end
-  protect_from_forgery with: :exception
+private
+ 
+	def current_ouser
+  		@current_ouser ||= Ouser.find_by(id: session[:ouser_id])
+	end
+ 
+	helper_method :current_ouser
 end
