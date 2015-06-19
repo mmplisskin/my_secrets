2a058145d3d70ea7fcc48626a5f5b5b4046be9b5
mmplisskin <mmplisskin@gmail.com>
2015-06-19 07:05:53 -0700

    adds jquery turbolinks for heroku test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,10 +11,10 @@
 // about supported directives.
 //
 //= require jquery
-
-//= require jquery_ujs
 //= require bootstrap-tagsinput
+//= require jquery_ujs
 
+//= require jquery.turbolinks
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require semantic-ui
