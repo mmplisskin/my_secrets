e7fa34dfb04ecb74aaf56ff4b4ef7746e4d39db6
mmplisskin <mmplisskin@gmail.com>
2015-06-24 02:25:48 -0700

    conditional js map

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -7,45 +7,44 @@ function initialize() {
   var url = window.location.origin + window.location.pathname + ".json";
 
   $.get(url, function(results){
-    console.log(results)
-    var post = results[0]["ouser"]
-    console.log(post)
+    // console.log(results)
+    if (results[0] == undefined ){
+      return
+
+    }
+    else{
+
+      var post = results[0]["ouser"]
+      // console.log(post)
 
 
-    var lat = post["latitude"];
-    var long = post["longitude"];
-    console.log(lat,long)
+      var lat = post["latitude"];
+      var long = post["longitude"];
+      console.log(lat,long)
 
-    var myCenter = new google.maps.LatLng(lat,long);
-    var marker;
+      var myCenter = new google.maps.LatLng(lat,long);
 
-    var mapProp = {
-      center: myCenter,
-      zoom:12,
-      zoomControl:false,
-      panControl:false,
-      mapTypeId:google.maps.MapTypeId.HYBRID
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+      var mapProp = {
+        center: myCenter,
+        zoom:12,
+        zoomControl:false,
+        panControl:false,
+        mapTypeId:google.maps.MapTypeId.HYBRID
+      };
+      var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 
-    var marker = new google.maps.Marker({
-      position:myCenter
+      var marker = new google.maps.Marker({
+        position:myCenter
 
-    });
+      });
 
-    marker.setMap(map);
+      marker.setMap(map);
 
-    google.maps.event.addListener(marker,'click', function() {
-      map.setZoom(16);
-      map.setCenter(marker.getPosition());
-    });
-
-    // var infowindow = new google.maps.InfoWindow({
-    //   // content: my_content
-    // });
-
-    // infowindow.open(map,marker);
-
-  })
+      google.maps.event.addListener(marker,'click', function() {
+        map.setZoom(16);
+        map.setCenter(marker.getPosition());
+      });
+  }
 }
+);}
 google.maps.event.addDomListener(window, 'load', initialize);
