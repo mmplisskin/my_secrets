55d1cd68c573fc56ec99856bfac2a3d9433e22c1
Busyra Anwar <busyra@me.com>
2015-06-26 16:21:37 -0700

    changes google scope to allow emails to be saved

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,20 +2,30 @@ require 'rails_helper'
 
 RSpec.describe PostsController, :type => :controller do
 
+  before :each do
+    @post1 = Post.create(title:"facebook", description: "Deleted it" )
+    @post2 = Post.create(title:"myspace", description: "No one cares")
+  end
+  let(:valid_attributes) do
+    {
+    :title => "Facebook",
+    :description => "Delete it",
+    }
+  end
+  describe "POST create" do
+    describe "without valid log in" do
+      it "sends user back to log in page" do
+        (expect(response.status).to eq(200))
 
-  # describe "POST create" do
-  #   describe "without valid log in" do
-  #     it "sends user back to log in page" do
-  #       expect(response).to redirect_to login_url
-  #     end
-  #   end
-  # end
+        end
+    end
+  end
 
-  #     it "assigns a newly created post as @post" do
-  #       post :create, {:post => valid_attributes}
-  #       expect(assigns(:post)).to be_a(post)
-        #expect(assigns(:post)).to be_persisted
-      # end
+      it "assigns a newly created post as @post" do
+        post :create, {:post => valid_attributes}
+        expect(assigns(:post)).to be_a(post)
+        expect(assigns(:post)).to be_persisted
+      end
 
       # it "redirects to the created post" do
       #   post :create, {:post => valid_attributes}
