163580af26468a50e338efb66ff097ec39727a76
mmplisskin <mmplisskin@gmail.com>
2015-08-30 15:22:53 -0700

    reverts assets compile issue with boootstrap gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,12 +14,6 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-
-  config.assets.css_compressor = :sass
-
-  config.assets.compile = true
-  config.assets.precompile =  ['*.js', '*.css', '*.css.erb']
-
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
@@ -35,7 +29,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  # config.assets.compile = false
+  config.assets.compile = false
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
