cfae02a5ecaf9757739236a4dd6daf3f1fb7b6e3
mmplisskin <mmplisskin@gmail.com>
2015-09-04 11:00:59 -0700

    checpoint before modifying oauth to remove duplicate accs

--- app/models/ouser.rb
+++ app/models/ouser.rb
@@ -7,7 +7,7 @@ class Ouser < ActiveRecord::Base
 
 	class << self
 	  def from_omniauth(auth_hash)
-	    ouser = find_or_create_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
+			ouser = find_or_create_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
 	    ouser.name = auth_hash['info']['name']
 	    ouser.location = auth_hash['info']['location']
 	    ouser.image_url = auth_hash['info']['image']
