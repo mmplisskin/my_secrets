f311616df214c37a32ad51bf1902c464bbc04663
mmplisskin <mmplisskin@gmail.com>
2015-09-05 23:03:51 -0700

    increases drift

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -45,7 +45,7 @@ require 'pry'
 		@ouser = Ouser.find(params[:id])
 		@token = (params[:token])
 		# binding.pry
-		if @ouser.authenticate_otp(@token, drift: 60)
+		if @ouser.authenticate_otp(@token, drift: 120)
 			  session[:ouser_id] = @ouser.id
 				redirect_to posts_path
 		else
